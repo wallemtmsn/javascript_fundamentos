4c558c327d971dbe2788807f43bc808d24730ad8
Wallemberg <wallemtmsn@hotmail.com>
2023-04-26 14:14:10 -0300

    master

---
+++ console.js
@@ -0,0 +1,18 @@
+// log  -> registro
+
+
+// const minhaVar = true
+
+// console.log(245);
+// console.log('Eu sou um texto')
+// console.log(minhaVar);
+// console.info('Mensagem informativa')
+
+
+function numeroPar(numero){
+    console.assert(numero % 2 === 0, 'Número não é par');
+    return numero % 2 === 0;
+}
+
+
+console.log(numeroPar(7))
---
+++ index.js
@@ -0,0 +1,41 @@
+// Rodar um serviço de envio de email
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/api/email', async (req, res) => {
+  const { to, subject, text } = req.body;
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.live.com',
+      port: 465,
+      secure: false,
+      auth: {
+        user: 'wallemtmsn@hotmail.com', //coloque seu e-mail
+        pass: '@#Kivy1310' //coloque sua senha
+      }
+    });
+
+    const info = await transporter.sendMail({
+      from: 'wallemtmsn@hotmail.com', //coloque seu e-mail
+      to: 'xheadshot@hotmail.com',
+      subject: 'Teste NodeJS',
+      text: 'Mensagem de exemplo'
+    });
+
+    res.send(info);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
