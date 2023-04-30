1278ea63bd781fe06154b5b7a4593eb0bc1f352b
Wallemberg <wallemtmsn@hotmail.com>
2023-04-30 11:01:54 -0300

    Trabahando com Template Literal

---
+++ template_strings.js
@@ -0,0 +1,14 @@
+const nome = 'Wallem'; 
+const apresentacao = 'O meu nome é ';
+const idade = 35;
+const cidade = 'Campos dos Goytacazes'
+
+// Formas de contatenação
+console.log(`O meu nome é ${nome}`);
+console.log(apresentacao + nome);
+console.log('O meu nome é ' + nome);
+
+
+// TEMPLATE LITERAL ou TEMPLATE STRINGS.
+
+console.log(`Olá, meu nome é ${nome} tenho ${idade} anos, e moro na cidade de ${cidade}.`)
