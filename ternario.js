c7e07ea6b30ba3487060705c6bc668d0942657a2
Wallemberg <wallemtmsn@hotmail.com>
2023-04-30 10:41:34 -0300

    Trabalhando com Operador Ternário

---
+++ ternario.js
@@ -0,0 +1,17 @@
+
+
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+// If clássico, modo normal de declarar uma condição.
+
+// if (idadeCliente >= idadeMinima) {
+//     console.log('Cerveja Liberada')
+// } else {
+//     console.log('Devido a sua idade não ser menor que 18, Liberaremos somente suco para este cliente.')
+// }
+
+// Fazendo a condição com Operador Ternário "?"
+
+                    // Condição             //tue                   //false
+console.log(idadeCliente >= idadeMinima ? 'Cerveja Liberada': 'Devido a sua idade não ser menor que 18, Liberaremos somente suco para este cliente.')
