47f0546721c496e7191250d6a6e58dddefcc3495
Wallemberg <wallemtmsn@hotmail.com>
2023-04-30 22:53:25 -0300

    Trabalhando com funções

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+// 1) forma de escrever função
+function imprimir(x) {
+    console.log(x)
+}
+
+imprimir(soma())
+
+// Outras formas de escrever função
+
+function soma() {
+    return 2 * 5;
+
+}
+//console.log(soma());
+
+
+// Valores armazenados em variáveis.
+
+let valor1 = 10
+let valor2 = 20
+
+function multiplicar() {
+    return valor1 * valor2;
+}
+console.log(multiplicar())
