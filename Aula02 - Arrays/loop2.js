4d11c87d0f4a13da6db9d1f0eedc01ecad2bd0f4
Wallemberg <wallemtmsn@hotmail.com>
2023-05-05 10:16:48 -0300

    Adicionado função de buscar alunos por nota

--- Aula02 - Arrays/loop2.js
+++ Aula02 - Arrays/loop2.js
@@ -3,22 +3,35 @@
 
 
 
-const alunos = ['Fernando', 'Gustavo', 'Olívia', 'Nina', 'Maria', 'Carlos']
-const mediaAlunos = [6, 7.4, 8, 6.8, 9.5, 9]
-
-const alunosEMedias = [alunos, mediaAlunos]
-
+const alunos = ['Fernando', 'Gustavo', 'Olívia', 'Nina', 'Maria', 'Carlos'];
+const mediaAlunos = [6, 7.4, 9, 6.8, 9.5, 9];
+const alunosEMedias = [alunos, mediaAlunos];
+// Buscando a média do aluno
 function buscarAluno(aluno) {
     if (alunosEMedias[0].includes(aluno)) {
-        buscarIndex = alunosEMedias[0].indexOf(aluno)
-        const mediaALuno = alunosEMedias[1][buscarIndex] 
+        buscarIndex = alunosEMedias[0].indexOf(aluno);
+        const mediaALuno = alunosEMedias[1][buscarIndex] ;
         console.log(`
-        Aluno: ${aluno}
-        Média: ${mediaALuno}
+        Aluno: ${aluno};
+        Média: ${mediaALuno};
         `)
     } else {
-        console.log(`O Aluno ${aluno} que você buscou não foi encontrado na lista de alunos`)
+        console.log(`O Aluno ${aluno} que você buscou não foi encontrado na lista de alunos`);
     }
 }
 
-buscarAluno('Olívia')
+// Função para buscar alunos com x de média
+function buscarPorMedia(media) {
+    const indices = [];
+    for (let i = 0; i < alunosEMedias[1].length; i++) {
+      if (alunosEMedias[1][i] === media) {
+        indices.push(i);
+      }
+    }
+    if (indices.length > 0) {
+      console.log(`Alunos encontrado com a média ${media}: ` + indices.map(i => alunosEMedias[0][i]));
+    } else {
+      console.log(`Média ${media} não encontrada`);
+    }
+  }
+buscarPorMedia(9)
